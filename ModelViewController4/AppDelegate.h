2cbfa9a6b2c5c9de3eec78c715d70b095f2b0d72
dominhhai1096 <dominhhai1096@gmail.com>
2015-12-24 10:05:15 +0700

    Initial Commit

---
+++ ModelViewController4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ModelViewController4
+//
+//  Created by Do Minh Hai on 12/24/15.
+//  Copyright (c) 2015 Do Minh Hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ModelViewController4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModelViewController4
+//
+//  Created by Do Minh Hai on 12/24/15.
+//  Copyright (c) 2015 Do Minh Hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
